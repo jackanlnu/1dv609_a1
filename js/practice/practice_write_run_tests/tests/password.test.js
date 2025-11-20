d7cd0e214de192a13249bb30f204fdfaf71380fa
Jack <jo223se@student.lnu.se>
2025-11-20 13:19:53 +0100

    Improve test names in password.test

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -16,33 +16,33 @@ describe('Password class, test suite', () => {
     //put constants here to increase readability
     const acceptablePassword = 'Password1234';
 
-    test('Should Hash Password', () => {
+    test('constructor Should Hash Password', () => {
         expect(new Password(acceptablePassword).getPasswordHash()).not.toBe('Password1234');
     });
 
-    test('Should Trim Password', () => {
+    test('constructor Should Trim Password', () => {
         const spacesPassword = new Password(' Password1234 ')
         expect(new Password(acceptablePassword).getPasswordHash()).toBe(spacesPassword.getPasswordHash());
     });
 
-    test('Should Return False if Diffrent Password', () => {
+    test('isPasswordSame Should Return False if Diffrent Password', () => {
         const diffrentPassword = new Password('Password12345')
         expect(new Password(acceptablePassword).isPasswordSame(diffrentPassword)).toBe(false);
     });
 
-    test('Should Throw Correct Error if Password Does Not Contain Number', () => {
+    test('constructor Should Throw Correct Error if Password Does Not Contain Number', () => {
         expect(() => new Password('Passwordtest')).toThrow('No number found');
     });
 
-    test('Should Throw Correct Error if Password Is too short', () => {
+    test('constructor Should Throw Correct Error if Password Is too short', () => {
         expect(() => new Password('Password123')).toThrow('Too short password');
     });
 
-    test('Should Not Throw Error if Password Meets Criteria', () => {
+    test('constructor Should Not Throw Error if Password Meets Criteria', () => {
         expect(() => new Password(acceptablePassword)).not.toThrow();
     });
 
-    test('Should Use Correct Hashing Algorithm', () => {
+    test('constructor Should Use Correct Hashing Algorithm', () => {
         expect(new Password(acceptablePassword).getPasswordHash()).toBe(7628963596958728000);
     });
 });
