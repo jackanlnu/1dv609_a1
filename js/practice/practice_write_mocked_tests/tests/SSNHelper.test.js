6517a66a7f1787052b8acb8a6c5795e7810f8eec
Jack <jo223se@student.lnu.se>
2025-11-21 12:56:06 +0100

    Add imports SNNHelper.test

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,5 +1,9 @@
 import { SSNHelper } from '../src/correct/SSNHelper'; 
-
+import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'; 
+import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth0'; 
+import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'; 
+import { SSNHelper } from '../src/bugs/BuggySSNHelperMessyLuhn'; 
+import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength'; 
 
 
 describe('SSNHelpe Tests', () => {
