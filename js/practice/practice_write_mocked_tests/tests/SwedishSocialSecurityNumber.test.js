ea28f8e140b8fbefe69a318e8db135b65a5bfed3
Jack <jo223se@student.lnu.se>
2025-11-24 12:28:33 +0100

    Add mock to SwedishSocialSecurityNumber.test

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,5 +1,7 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
+import { jest } from '@jest/globals';
+
+// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
@@ -7,9 +9,16 @@ import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurit
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
     //put constants here to increase readability
+    const mock = {
+        isCorrectLength: jest.fn(),
+        isCorrectFormat: jest.fn(),
+        isValidMonth: jest.fn(),
+        isValidDay: jest.fn(),
+        luhnisCorrect: jest.fn()
+    }
 
     test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
+        expect(() => new SwedishSocialSecurityNumber('050210-0000', mock)).not.toThrow();
     });
 
     //Add your tests here
