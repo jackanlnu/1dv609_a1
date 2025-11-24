0f74bc94895e4634c7afae0bb7bb7610d3defb23
Jack <jo223se@student.lnu.se>
2025-11-24 11:14:32 +0100

    Add isValidMonth Should Return False For Invalid Input in SNNHelper.test

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,6 +1,6 @@
-import { SSNHelper } from '../src/correct/SSNHelper'; 
+// import { SSNHelper } from '../src/correct/SSNHelper'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'; 
-// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth0'; 
+import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth0'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperMessyLuhn'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength'; 
@@ -10,6 +10,10 @@ describe('SSNHelpe Tests', () => {
 
     test('isValidDay Should Return True For Valid Day', () => {
         expect(new SSNHelper().isValidDay(31)).toBe(true);
+    });
+
+    test('isValidMonth Should Return False For Invalid Input', () => {
+        expect(new SSNHelper().isValidMonth(0)).toBe(false);
     });
 
     test('isCorrectFormat Should Return False For Invalid Format', () => {
