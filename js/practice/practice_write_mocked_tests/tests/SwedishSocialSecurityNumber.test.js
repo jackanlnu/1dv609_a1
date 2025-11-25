b6d2cbd2258d479fc3937679841df323b75ac483
Jack <jo223se@student.lnu.se>
2025-11-25 13:03:17 +0100

    Fix constructor Should Throw Error For Too Long Security Number in SwedishSocialSecurityNumber.test

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,7 +1,7 @@
 import { jest } from '@jest/globals';
 
-// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
-import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
+import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
@@ -9,16 +9,16 @@ import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecur
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
     //put constants here to increase readability
-    const mock = {
-        isCorrectLength: jest.fn(),
-        isCorrectFormat: jest.fn(),
-        isValidMonth: jest.fn(),
-        isValidDay: jest.fn(),
-        luhnisCorrect: jest.fn()
-    }
 
     test('constructor Should Throw Error For Too Long Security Number', () => {
-        expect(() => new SwedishSocialSecurityNumber('050210-00000', mock)).toThrow();
+        const mock = {
+            isCorrectLength: jest.fn().mockReturnValue(false),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn(),
+            isValidDay: jest.fn(),
+            luhnisCorrect: jest.fn()
+        }
+        expect(() => new SwedishSocialSecurityNumber('050210-00000', mock)).toThrow('To short, must be 11 characters');
     });
 
     //Add your tests here
