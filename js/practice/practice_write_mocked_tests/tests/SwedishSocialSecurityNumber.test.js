f8f60ba0dec61bf7a986a53b8b88dcbb5576af8e
Jack <jo223se@student.lnu.se>
2025-11-25 13:26:01 +0100

    Add constructor Should Trim Security Number Input to SwedishSocialSecurityNumber.test

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,8 +1,8 @@
 import { jest } from '@jest/globals';
 
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
+import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
 
@@ -19,6 +19,20 @@ describe('SwedishSocialSecurityNumber Tests', () => {
             luhnisCorrect: jest.fn()
         }
         expect(() => new SwedishSocialSecurityNumber('050210-00000', mock)).toThrow('To short, must be 11 characters');
+    });
+
+    test('constructor Should Trim Security Number Input', () => {
+        const mock = {
+            isCorrectLength: jest.fn(),
+            isCorrectFormat: jest.fn(),
+            isValidMonth: jest.fn(),
+            isValidDay: jest.fn(),
+            luhnisCorrect: jest.fn()
+        }
+        const spacedSecurityNumber = '  050210-0000  '
+        const SecurityNumber = new SwedishSocialSecurityNumber(spacedSecurityNumber, mock)
+        expect(SecurityNumber.getYear()).toBe('05');
+        expect(SecurityNumber.getSerialNumber()).toBe('0000');
     });
 
     //Add your tests here
