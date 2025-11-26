55e1c87ae6e44b53c9de6d03f33024c42dcef007
Jack <jo223se@student.lnu.se>
2025-11-26 12:17:54 +0100

    Refactor SwedishSocialSecurityNumber.test

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,85 +1,54 @@
-import { jest } from '@jest/globals';
+import { beforeEach, jest } from '@jest/globals'
 
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongMonth'; 
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongDay'; 
+import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn' 
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongMonth' 
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongDay' 
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
+    let helperMock
+
+    beforeEach(() => {
+        helperMock = {
+            isCorrectLength: jest.fn(),
+            isCorrectFormat: jest.fn(),
+            isValidMonth: jest.fn(),
+            isValidDay: jest.fn(),
+            luhnisCorrect: jest.fn()
+        }
+    })
 
     test('constructor Should Throw Error For Too Long Social Security Number', () => {
-        const mock = {
-            isCorrectLength: jest.fn().mockReturnValue(false),
-            isCorrectFormat: jest.fn().mockReturnValue(true),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn()
-        }
-        expect(() => new SwedishSocialSecurityNumber('050210-00000', mock)).toThrow('To short, must be 11 characters');
-    });
+        helperMock.isCorrectLength.mockReturnValue(false)
+        helperMock.isCorrectFormat.mockReturnValue(true)
+        expect(() => new SwedishSocialSecurityNumber('050210-00000', helperMock)).toThrow('To short, must be 11 characters')
+    })
 
     test('constructor Should Trim Social Security Number Input', () => {
-        const mock = {
-            isCorrectLength: jest.fn(),
-            isCorrectFormat: jest.fn(),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn()
-        }
         const spacedSecurityNumber = '  050210-0000  '
-        const SecurityNumber = new SwedishSocialSecurityNumber(spacedSecurityNumber, mock)
-        expect(SecurityNumber.getYear()).not.toMatch(' ');
-        expect(SecurityNumber.getSerialNumber()).not.toMatch(' ');
-    });
+        const SecurityNumber = new SwedishSocialSecurityNumber(spacedSecurityNumber, helperMock)
+        expect(SecurityNumber.getYear()).not.toMatch(' ')
+        expect(SecurityNumber.getSerialNumber()).not.toMatch(' ')
+    })
 
     test('constructor Should Throw Error For Incorrect Luhn', () => {
-        const mock = {
-            isCorrectLength: jest.fn(),
-            isCorrectFormat: jest.fn(),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn().mockReturnValue(false),
-        }
-        expect(() => new SwedishSocialSecurityNumber('050210-0000', mock)).toThrow("Invalid SSN according to Luhn's algorithm");
-    });
+        helperMock.luhnisCorrect.mockReturnValue(false)
+        expect(() => new SwedishSocialSecurityNumber('050210-0000', helperMock)).toThrow("Invalid SSN according to Luhn's algorithm")
+    })
 
     test('getYear Should Return Social Security Number Year', () => {
-        const mock = {
-            isCorrectLength: jest.fn(),
-            isCorrectFormat: jest.fn(),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn()
-        }
-        expect(new SwedishSocialSecurityNumber('050210-0000', mock).getYear()).toBe('05');
-    });
+        expect(new SwedishSocialSecurityNumber('050210-0000', helperMock).getYear()).toBe('05')
+    })
 
     test('getMonth Should Return Social Security Number Month', () => {
-        const mock = {
-            isCorrectLength: jest.fn(),
-            isCorrectFormat: jest.fn(),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn()
-        }
-        expect(new SwedishSocialSecurityNumber('050210-0000', mock).getMonth()).toBe('02');
-    });
+        expect(new SwedishSocialSecurityNumber('050210-0000', helperMock).getMonth()).toBe('02')
+    })
 
     test('getDay Should Return Social Security Number Day', () => {
-        const mock = {
-            isCorrectLength: jest.fn(),
-            isCorrectFormat: jest.fn(),
-            isValidMonth: jest.fn(),
-            isValidDay: jest.fn(),
-            luhnisCorrect: jest.fn()
-        }
-        expect(new SwedishSocialSecurityNumber('050210-0000', mock).getDay()).toBe('10');
-    });
-
-    //Add your tests here
-});
+        expect(new SwedishSocialSecurityNumber('050210-0000', helperMock).getDay()).toBe('10')
+    })
+})
